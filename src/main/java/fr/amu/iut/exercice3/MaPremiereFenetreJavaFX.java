983663562d4ebe0c5d29ae81358f44fc5d1d0c3e
SIMON-Louis-2326101aa <louis.simon.3@etu.univ-amu.fr>
2024-04-15 09:50:32 +0200

    Exo 3 finis mais j'ai pas l'image du boutton donc bon...

--- src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
+++ src/main/java/fr/amu/iut/exercice3/MaPremiereFenetreJavaFX.java
@@ -1,6 +1,14 @@
 package fr.amu.iut.exercice3;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class MaPremiereFenetreJavaFX extends Application {
@@ -11,7 +19,32 @@ public class MaPremiereFenetreJavaFX extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("La page d'un Pro de JavaFX");
+
+        VBox vbox = new VBox();
+        vbox.setAlignment( Pos.CENTER );
+
+        Label txtBjr = new Label("Bonjour à tous !");
+        vbox.getChildren().add( txtBjr );
+
+        TextField nameField = new TextField("Veuillez saisir un nom");
+        nameField.setMaxWidth(180.0d);
+        nameField.setFont( Font.font("Courier", FontWeight.NORMAL, 12) );
+        vbox.getChildren().add( nameField );
+
+        Button button = new Button("Dire bonjour !");
+        vbox.getChildren().add( button );
+
+        button.addEventHandler(MouseEvent.MOUSE_CLICKED, actionEvent -> {
+            txtBjr.setText( "Bonjour à toi, " + nameField.getText() + " !" );
+        });
+
+        //D'autres options de codage sonts sur le tp 2.1
+
+        Scene sc1 = new Scene(vbox);
+        primaryStage.setWidth( 400 );
+        primaryStage.setHeight( 400 );
+        primaryStage.setScene(sc1);
+        primaryStage.setTitle("Hello application");
         primaryStage.show();
     }
 }
