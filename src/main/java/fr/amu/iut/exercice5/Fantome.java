f580ba56b051899e8ca3a5ff2344e489e1f18f74
SIMON-Louis-2326101aa <louis.simon.3@etu.univ-amu.fr>
2024-04-18 08:58:19 +0200

    Fantom finis

--- src/main/java/fr/amu/iut/exercice5/Fantome.java
+++ src/main/java/fr/amu/iut/exercice5/Fantome.java
@@ -39,23 +39,47 @@ public class Fantome extends Personnage {
 
     @Override
     public void deplacerAGauche() {
+        super.deplacerAGauche();
 
+        retineGauche.setCenterX(oeilGauche.getCenterX() - 1);
+        retineGauche.setCenterY(oeilGauche.getCenterY());
+
+        retineDroite.setCenterX(oeilDroit.getCenterX() - 1);
+        retineDroite.setCenterY(oeilDroit.getCenterY());
 
     }
 
     @Override
     public void deplacerADroite(double largeurJeu) {
+        super.deplacerADroite(largeurJeu);
 
+        retineGauche.setCenterX(oeilGauche.getCenterX() + 1);
+        retineGauche.setCenterY(oeilGauche.getCenterY());
+
+        retineDroite.setCenterX(oeilDroit.getCenterX() + 1);
+        retineDroite.setCenterY(oeilDroit.getCenterY());
     }
 
     @Override
     public void deplacerEnBas(double hauteurJeu) {
+        super.deplacerEnBas(hauteurJeu);
 
+        retineGauche.setCenterX(oeilGauche.getCenterX());
+        retineGauche.setCenterY(oeilGauche.getCenterY() + 1);
+
+        retineDroite.setCenterX(oeilDroit.getCenterX());
+        retineDroite.setCenterY(oeilDroit.getCenterY() + 1);
     }
 
     @Override
     public void deplacerEnHaut() {
+        super.deplacerEnHaut();
 
+        retineGauche.setCenterX(oeilGauche.getCenterX());
+        retineGauche.setCenterY(oeilGauche.getCenterY() - 1);
+
+        retineDroite.setCenterX(oeilDroit.getCenterX());
+        retineDroite.setCenterY(oeilDroit.getCenterY() - 1);
     }
 
 
