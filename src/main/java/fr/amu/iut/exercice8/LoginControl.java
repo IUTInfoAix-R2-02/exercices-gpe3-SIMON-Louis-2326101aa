ea9fc739fb14dcf2f601fca25986959866d8f632
SIMON-Louis-2326101aa <louis.simon.3@etu.univ-amu.fr>
2024-05-22 10:01:00 +0200

    Exrcice 8 en cours de modif de boutons

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,32 @@
 package fr.amu.iut.exercice8;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class LoginControl extends GridPane {
 
+    @FXML
+    Label usernameLabel;
+    @FXML
+    Label passwordLabel;
+    @FXML
+    Button loginButton;
+    @FXML
+    Button cancelButton;
+    @FXML
+    TextField usernameField;
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
     private void okClicked() {
     }
 
+    @FXML
     private void cancelClicked() {
     }
 }
